f30877b00f33176bfb2716f881c2ec62acb3b4d4
Maria Denisova <artftw@mail.ru>
2022-09-14 01:01:32 +0300

    add .gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int balance = 200;
         int money = 1100;
-        int bonus = money/100;
+        int bonus = money / 100;
 
 
-        if (money > 1000)  {
+        if (money > 1000) {
             System.out.println(" Бонус " + bonus);
             System.out.println(" Баланс " + (balance + money + bonus));
-        }
-        if (money < 1000)  {
+        } else {
             System.out.println(" Бонус 0 ");
             System.out.println(" Баланс " + (balance + money));
 
